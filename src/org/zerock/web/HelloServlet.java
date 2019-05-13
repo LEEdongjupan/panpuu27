eac2e757299869ed1fdae50539bffa6c4b3097ba
bitcamp07 <bitcamp07@bitcamp>
2019-05-13 09:31:54 +0900

    First Commit

---
+++ src/org/zerock/web/HelloServlet.java
@@ -0,0 +1,86 @@
+package org.zerock.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.Service;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	private int count = 0 ;
+	
+	
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+//
+//	@Override
+//	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		System.out.println("Service......");
+//		
+	
+//	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init.....");
+		System.out.println("init.....");
+		System.out.println("init.....");
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init.....1");
+		System.out.println("init.....1");
+		System.out.println("init.....1");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		String name = request.getParameter("name");
+		
+		System.out.println(name);
+		response.setContentType("text/html; charset=UTF-8");
+		response.getWriter().print(name);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
